6100797ca279aa9021ca1bca880783e5f6533ee4
javad-kuttappi <javaadde123@hotmail.com>
2025-06-27 10:59:32 +0530

    added the second section (intro) and compleated the resposive of it

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 
 
 function navClose(){
-
+   document.getElementById("links").style.display="none"
+   document.getElementById("nav-open").style.display="flex"
+   document.getElementById("nav-close").style.display="none"
 }
 
 function navOpen(){
    document.getElementById("links").style.display="flex"
+   document.getElementById("nav-open").style.display="none"
+   document.getElementById("nav-close").style.display="flex"
 }
