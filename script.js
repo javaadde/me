43ded6a16a0fc933dc3494f51572ea7e43ea8d58
javad-kuttappi <javaadde123@hotmail.com>
2025-06-28 17:27:45 +0530

    compleated the project have to add some photos and videos in it

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 function navClose(){
    document.getElementById("links").style.display="none"
    document.getElementById("nav-open").style.display="flex"
@@ -12,3 +10,68 @@ function navOpen(){
    document.getElementById("nav-open").style.display="none"
    document.getElementById("nav-close").style.display="flex"
 }
+
+
+    let vid = document.getElementById("myVideo");
+
+    function playVid(){
+      vid.play();
+      document.getElementById("play").style.visibility="hidden"
+      document.getElementById("pause").style.visibility="visible"
+      console.log("worked");
+    }
+
+    function pauseVid(){
+       vid.pause();
+      document.getElementById("play").style.visibility="visible"
+      document.getElementById("pause").style.visibility="hidden";
+    }
+
+
+const swiper = new Swiper('.swiper', {
+
+   grabCursor:true,
+   centeredSlides:true,
+   speed:1200,
+   spaceBetween:600,
+   slidesPerView:"auto",
+   effect:"coverflow",
+   
+   coverflowEffect:{
+    rotate:0,
+    stretch:0,
+    depth:150,
+    modifier:1,
+    slideShadows:false,
+   },
+
+   pagination:{
+     el:".swiper-pagination",
+   },
+
+   // mousewheel:{
+   //  invert:false,
+   //  thresholdDelta:50,
+   //  sensitivity:1,
+   // },
+
+
+   breakpoints:{
+      260:{
+         spaceBetween:30,
+      },
+      760:{
+         spaceBetween:40,
+      },
+   },
+
+
+
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
+
